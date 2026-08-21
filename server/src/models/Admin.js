ce4d830b6a35be3b5325f8c5602097af938e8117
Bhaumik-05 <modibhaumik180@gmail.com>
2026-08-21 19:10:52 +0530

    [MODELS] User and Admin

---
+++ server/src/models/Admin.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const admin = new Schema
+( 
+    {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Admin = model("Admin", admin);
+
+export default Admin;
