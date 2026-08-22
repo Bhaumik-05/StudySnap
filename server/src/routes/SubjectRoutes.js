78723d44926ad984de39149f9a3bcd09a105cbcd
Raj Mevawala <rajmevawala23@icloud.com>
2026-08-22 19:27:25 +0530

    [IMP] authorization for subjects

--- server/src/routes/SubjectRoutes.js
+++ server/src/routes/SubjectRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { getSubjects, createSubject, updateSubject, deleteSubject } from "../controller/SubjectController.js";
 
+import authMiddleware from "../middleware/authMiddleware.js";
+import roleMiddleware from "../middleware/roleMiddleware.js";
+
 const router = express.Router();
 
 router.get("/", getSubjects);
-router.post("/", createSubject);
-router.patch("/:id", updateSubject);
-router.delete("/:id", deleteSubject);
+router.post("/", authMiddleware, roleMiddleware("ADMIN"), createSubject);
+router.patch("/:id", authMiddleware, roleMiddleware("ADMIN"), updateSubject);
+router.delete("/:id", authMiddleware, roleMiddleware("ADMIN"), deleteSubject);
 
 export default router;
