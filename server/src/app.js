90c66962312e6dc8ae9de569d8acd6765c89b180
Raj Mevawala <rajmevawala23@icloud.com>
2026-09-26 21:40:57 +0530

    connected backend with frontend

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,12 @@ import downloadHistoryRoutes from "./routes/downloadHistoryRoutes.js"
 import adminRoutes from "./routes/AdminRoutes.js";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
